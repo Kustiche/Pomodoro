d42d9e4c51b46f06e5ac052d8564d682df0df8dd
Kustiche <ChernichnyKustik@mail.ru>
2025-07-01 16:50:15 +0300

    Реализовал смену таймера при нажатии кнопок, совместил смену по кнопкам с автоматической сменой таймера

--- src/js/main.js
+++ src/js/main.js
@@ -9,6 +9,7 @@ class Pomodoro {
     this.interval = null;
     this.typeTimer = 'pomodoro';
     this.numIterations = 0;
+    this.numShiftsTimers = 0;
 
     this.timer = document.querySelector('.pomodoro__timer');
     this.btnStart = document.querySelector('.functional__start');
@@ -17,11 +18,17 @@ class Pomodoro {
     this.inputPomodoro = document.querySelector('.form__input-pomodoro');
     this.inputShort = document.querySelector('.form__input-short');
     this.inputLong = document.querySelector('.form__input-long');
+    this.btnPomodoro = document.querySelector('.types-timers__pomodoro');
+    this.btnShort = document.querySelector('.types-timers__short');
+    this.btnLong = document.querySelector('.types-timers__long');
+    this.innerTypesTimers = document.querySelector('.pomodoro__types-timers');
+
+    this.btnsPomodoro = document.querySelectorAll('.pomodoro-btn');
 
     this.modalSettings = document.querySelector('#modalSettings');
 
     this.startListeners();
-    this.updateMinutes();
+    this.updateMinutes('pomodoro');
   }
 
   startListeners() {
@@ -40,7 +47,7 @@ class Pomodoro {
     });
 
     this.btnReset.addEventListener('click', () => {
-      this.updateMinutes();
+      this.updateMinutes('pomodoro');
       this.stop();
 
       this.btnStart.classList.remove('active');
@@ -63,54 +70,125 @@ class Pomodoro {
 
       this.btnStart.classList.remove('active');
     });
+
+    this.innerTypesTimers.addEventListener('click', (e) => {
+      const isBtnPomodoro = e.target === this.btnPomodoro;
+      const isBtnShort = e.target === this.btnShort;
+      const isBtnLong = e.target === this.btnLong;
+
+      this.btnsPomodoro.forEach((el) => {
+        el.classList.remove('activeTimer');
+      });
+
+      if (isBtnPomodoro) {
+        this.btnPomodoro.classList.add('activeTimer');
+
+        this.switchTimer('pomodoro');
+        this.updateMinutes('pomodoro');
+      } else if (isBtnShort) {
+        this.btnShort.classList.add('activeTimer');
+
+        this.switchTimer('short');
+        this.updateMinutes('short');
+      } else if (isBtnLong) {
+        this.btnLong.classList.add('activeTimer');
+
+        this.switchTimer('long');
+        this.updateMinutes('long');
+      }
+
+      this.stop();
+      this.renderBtnTypesTimers();
+      this.btnStart.classList.remove('active');
+    });
   }
 
   start() {
     this.interval = setInterval(() => {
-      const minutes = this.minutes >= 10 ? this.minutes : `0${this.minutes}`;
-      const seconds = this.seconds >= 10 ? this.seconds : `0${this.seconds}`;
-
       if (this.seconds === 0 && this.minutes !== 0) {
         this.minutes = --this.minutes;
         this.seconds = 59;
+
+        this.renderTimer();
       } else if (this.seconds !== 0) {
         this.seconds = --this.seconds;
+
+        this.renderTimer();
       } else if (this.minutes === 0 && this.seconds === 0) {
-        this.timer.textContent = `${minutes}:${seconds}`;
+        this.renderTimer();
 
         this.switchTimer(this.typeTimer);
       }
-
-      this.timer.textContent = `${minutes}:${seconds}`;
     }, 1000);
   }
 
+  renderTimer() {
+    const minutes = this.minutes >= 10 ? this.minutes : `0${this.minutes}`;
+    const seconds = this.seconds >= 10 ? this.seconds : `0${this.seconds}`;
+
+    this.timer.textContent = `${minutes}:${seconds}`;
+  }
+
   switchTimer(type) {
-    if (type === 'pomodoro') {
-      if (this.numIterations !== 3) {
-        this.typeTimer = 'short';
-        this.minutes = this.short;
-      } else {
-        this.typeTimer = 'long';
-        this.minutes = this.long;
-      }
+    switch (type) {
+      case 'pomodoro':
+        if (this.numIterations === 3) {
+          this.typeTimer = 'long';
+          this.minutes = this.long;
+        } else if (this.numShiftsTimers === 0) {
+          this.typeTimer = 'short';
+          this.minutes = this.short;
 
-      this.stop();
-      this.start();
-    } else if (type === 'short') {
-      ++this.numIterations;
-      this.typeTimer = 'pomodoro';
-      this.minutes = this.pomodoro;
+          ++this.numShiftsTimers;
+        } else if (this.numShiftsTimers === 1) {
+          this.typeTimer = 'pomodoro';
+          this.minutes = this.pomodoro;
 
-      this.stop();
-      this.start();
-    } else if (type === 'long') {
-      this.typeTimer = 'pomodoro';
-      this.minutes = this.pomodoro;
-      this.numIterations = 0;
+          --this.numShiftsTimers;
+        } else if (this.numShiftsTimers === 2) {
+          this.numShiftsTimers = 0;
 
-      this.stop();
-      this.start();
+          ++this.numIterations;
+        }
+
+        break;
+      case 'short':
+        if (this.numShiftsTimers === 0 && this.numShiftsTimers === 2) {
+          this.typeTimer = 'short';
+          this.minutes = this.short;
+
+          --this.numShiftsTimers;
+        } else {
+          this.typeTimer = 'pomodoro';
+          this.minutes = this.pomodoro;
+
+          this.numShiftsTimers = 2;
+        }
+
+        break;
+      case 'long':
+        this.typeTimer = 'pomodoro';
+        this.minutes = this.pomodoro;
+        this.numIterations = 0;
+        this.numShiftsTimers = 0;
+
+        break;
+    }
+
+    this.renderBtnTypesTimers();
+  }
+
+  renderBtnTypesTimers() {
+    this.btnsPomodoro.forEach((el) => {
+      el.classList.remove('activeTimer');
+    });
+
+    if (this.typeTimer === 'pomodoro') {
+      this.btnPomodoro.classList.add('activeTimer');
+    } else if (this.typeTimer === 'short') {
+      this.btnShort.classList.add('activeTimer');
+    } else if (this.typeTimer === 'long') {
+      this.btnLong.classList.add('activeTimer');
     }
   }
 
@@ -118,15 +196,20 @@ class Pomodoro {
     clearInterval(this.interval);
   }
 
-  updateMinutes() {
-    this.typeTimer = 'pomodoro';
+  updateMinutes(type) {
+    this.typeTimer = type;
     this.numIterations = 0;
-    this.minutes = this.pomodoro;
     this.seconds = 0;
 
-    const minutes = this.minutes >= 10 ? this.minutes : `0${this.minutes}`;
+    if (type === 'pomodoro') {
+      this.minutes = this.pomodoro;
+    } else if (type === 'short') {
+      this.minutes = this.short;
+    } else if (type === 'long') {
+      this.minutes = this.long;
+    }
 
-    this.timer.textContent = `${minutes}:00`;
+    this.renderTimer();
   }
 
   updateParameters() {
